b475941f52892445fb4536732684ab5436a315ee
GorlovD <69232327+GorlovD@users.noreply.github.com>
2020-08-05 12:27:33 +0700

    Add files via upload

---
+++ Duplicate encoder/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text.ToLower();
+            for (int i = 0; i < str.Length; i++)
+            {
+                int j = i + 1;
+                while (j < str.Length)
+                {
+                    if (str[i] == str[j])
+                    {
+                        str = str.Replace(str[i].ToString(), ")");
+                    }
+                    j++;
+                }
+            }
+            for (int i = 0; i < str.Length; i++)
+                if (str[i].ToString() != ")")
+                {
+                    str = str.Replace(str[i].ToString(), "(");
+                }
+
+            label2.Text = str;
+
+        }
+
+        }
+    }
+
